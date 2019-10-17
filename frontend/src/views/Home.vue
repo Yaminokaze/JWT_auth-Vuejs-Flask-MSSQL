6060e87ee578cb41a76d50223c8c74eb260b27c1
Jihed Mastouri <25284659+Yaminokaze@users.noreply.github.com>
2019-10-17 05:15:55 +0200

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="col-sm-6 col-sm-offset-3">
-    <h1>Get a Free Chuck Norris Quote!</h1>
-    <button class="btn btn-primary" v-on:click="getQuote()">Get a Quote</button>
-    <div class="quote-area" v-if="quote">
-      <h2><blockquote>{{ quote }}</blockquote></h2>      
+    <h1>Welcome {{credentials.username}}!</h1>
+     <h2> Email: {{credentials.email}} </h2>
+     <h2> Country: {{credentials.country}} </h2>
+     <h2> birthdayDate: {{credentials.birthdayDate}} </h2>
     </div>
   </div>
 </template>
@@ -14,15 +14,20 @@ import auth from "@/auth/index";
 export default {
   data() {
     return {
-      quote: ""
+      credentials: {
+        username: "",
+        email: "",
+        country: "",
+        birthdayDate: "",
+      },
     };
   },
 
   methods: {
-    getQuote() {
+    mounted() {
       this.$http
         .get('http://localhost:/5000/home', (data) => {
-          this.quote = data;
+          this.credentials = data;
         }, { 
           headers: auth.getAuthHeader()
         })
